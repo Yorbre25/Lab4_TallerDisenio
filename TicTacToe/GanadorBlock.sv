67cdb45fdbbe453c19742fcddd509ba959ed7599
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-09-27 10:13:15 -0600

    WinnerBlock

--- TicTacToe/GanadorBlock.sv
+++ TicTacToe/GanadorBlock.sv
@@ -1,39 +1,40 @@
-module GanadorBlock(input clk, rst, input [3:0]entrada, revisar, output gana);
-	logic [] salida
+module GanadorBlock(input clk, rst, input [3:0]posicion, revisar, output gana);
+	logic [7:0] entrada;
+	logic [7:0] ganaAux;
+	
+	
+	Ganador fila0(clk, rst, entrada[0], ganaAux[0]);//0-1-2
+	Ganador fila1(clk, rst, entrada[1], ganaAux[1]);//3-4-5
+	Ganador fila2(clk, rst, entrada[2], ganaAux[2]);//6-7-8
+
+	Ganador column0(clk, rst, entrada[3], ganaAux[3]);//0-3-6
+	Ganador column1(clk, rst, entrada[4], ganaAux[4]);//1-4-7
+	Ganador column2(clk, rst, entrada[5], ganaAux[5]);//2-5-8
+	
+	Ganador diag0(clk, rst, entrada[6], ganaAux[6]);//0-4-8
+	Ganador diag1(clk, rst, entrada[7], ganaAux[7]);//2-4-6
 	
 	always_comb
-		case (entrada)
-			0: 
-				if(entrada == 0)
-					begin
-					entrada0 = entrada
-					entrada4 = entrada
-					entrada6 = entrada
-					end
-			1:
-			2:
-			3:
-			4:
-			5:
-			6:
-			7:
-			8:
+		case (posicion)
+			0: entrada = 8'b01001001;
+			1: entrada = 8'b00010001;
+			2:	entrada = 8'b10100001;
+			3: entrada = 8'b00001010;
+			4: entrada = 8'b11010010;
+			5: entrada = 8'b00100010;
+			6: entrada = 8'b10001100;
+			7: entrada = 8'b00010100;
+			8: entrada = 8'b01100100;
+			default: entrada = 0;
 		
 		endcase
-	
-	
-	always
-
-	Ganador fila0(clk, rst, entrada0, gana);//0-1-2
-	Ganador fila1(clk, rst, entrada1, gana);//3-4-5
-	Ganador fila2(clk, rst, entrada2, gana);//6-7-8
-
-	Ganador column0(clk, rst, entrada3, gana);//0-3-6
-	Ganador column1(clk, rst, entrada4, gana);//1-4-7
-	Ganador column2(clk, rst, entrada5, gana);//2-5-8
-	
-	Ganador diag0(clk, rst, entrada6, gana);//0-4-8
-	Ganador diag1(clk, rst, entrada7, gana);//2-4-6
-	
+		
+	always_comb
+		begin
+		if(ganaAux[0] | ganaAux[1] | ganaAux[2] | ganaAux[3] | ganaAux[4] | ganaAux[5] | ganaAux[6] | ganaAux[7])
+			gana = 1;
+		else
+			gana = 0;
+		end
 
 endmodule 
